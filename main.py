ebdd40a268c55728f9146af6bfa14f827e0cccaa
ichasada <danangpostman37@gmail.com>
2024-05-20 20:17:18 +0700

    add: author and add more title

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils.model import predict, load
 
-st.title("Social Media Usage and Emotional Well-Being")
+st.title("Social Media Usage and Emotional Well-Being | ML | Random Forest Classifier")
 st.caption("Mengklasifikasi Emosi Dominan pengguna berdasarkan aktifitas mereka dalam bersosial media.")
 
 col1, col2 = st.columns(2)
@@ -31,7 +31,7 @@ data = {
 model = load("./model/rf_model.pkl")
 
 # Click Event
-if col1.button("Diagnose"):
+if st.button("Diagnose"):
     st.subheader("Result", divider="violet")
     
     pred, chart_data = predict(data, model)
@@ -40,4 +40,6 @@ if col1.button("Diagnose"):
     st.write(f"You Seem : **{pred[0]}**")
     st.write("##### With Probability")
     st.bar_chart(chart_data)
+    
+    st.write("by @nangdosan")
  
