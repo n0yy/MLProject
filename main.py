77b393f24c5e52cf955b2f5150f9f3a8b8fbf60a
ichasada <danangpostman37@gmail.com>
2024-05-27 13:46:57 +0700

    update: mengupdate untuk tugas KWU

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import streamlit as st
 from src.model import predict, load
 from src.llm import make_prompt, get_summary
 
-st.title("Menganalisis Emosi Seseorang dalam Bersosial Media :iphone: | ML + LLM | Random Forest Classifier :deciduous_tree: :palm_tree: ")
-st.caption("Mengklasifikasi Emosi Dominan pengguna berdasarkan aktifitas mereka dalam bersosial media.")
+st.image("src/assets/LOGO.png")
+st.title("KENALI EMOSIMU SAAT BER-MEDIA SOSIAL")
+st.caption("Psychobot adalah sebuah sistem AI yang membantu kamu mengenali kondisi emosi dominan berdasarkan aktivitasmu di media sosial.")
+st.text("")
+st.text("")
+st.write("#### # Coba Sekarang dengan mengisi form dibawah ini :writing_hand:")
+st.text("")
 
 col1, col2 = st.columns(2)
 
@@ -17,6 +22,8 @@ daily_post = col2.number_input("Masukkan Jumlah Postingan per hari", 0, 256, 0)
 daily_likes = col2.number_input("Masukkan Jumlah Suka yang Diterima per hari", 0, 10_000, 0)
 daily_comment = col2.number_input("Masukkan Jumlah Komentar yang Diterima per hari", 0, 1_000, 0)
 daily_messages = col2.number_input("Masukkan Jumlah Pesan yang Diterima per hari", 0, 1_000, 0)
+
+
 
 data = {
     "Age": [age],
@@ -32,7 +39,8 @@ data = {
 model = load("./model/rf_model.pkl")
 
 # Click Event
-if st.button("Diagnose"):
+st.text("")
+if st.button("Analisis Sekarang!", type="primary"):
     st.subheader("Hasil", divider="violet")
     
     pred, chart_data = predict(data, model)
@@ -46,7 +54,4 @@ if st.button("Diagnose"):
         prompt = make_prompt(chart_data)
         md = get_summary(prompt)
 
-    st.markdown(md) 
-
-    st.write("built by @TnT_Team with :purple_heart:")
- 
+    st.markdown(md)  
